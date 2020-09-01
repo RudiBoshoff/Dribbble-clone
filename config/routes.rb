eeae143222c3adf21bc346abfaf35f11943b0e00
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-09-01 12:37:35 +0200

    improves styling and improves navigation with dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :shots
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "shots#index";
+  root "shots#index"
+  get 'profile/:id', to: "users#index", as: "profile"
 end
