83b7b500a2b65b799c5de0379f172a7e4d843118
Rudi Boshoff <RudiBoshoff@gmail.com>
2020-09-16 16:19:34 +0200

    improve preview loader fuctionality, reduce load times and remove gif uploading

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,4 +1,7 @@
 class Shot < ApplicationRecord
   belongs_to :user
   mount_uploader :user_shot, UserShotUploader
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 250 }
+  validates :user_shot, presence: true
 end
